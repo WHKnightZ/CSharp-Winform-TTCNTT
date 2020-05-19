b85d40c6643a07f9a5b552bbf7a83c37a85c6c6d
Dealer <nguyenkhanhsl1997@gmail.com>
2020-05-19 22:53:18 +0700

    1st

---
+++ TTCNTT/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace TTCNTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpControl = new System.Windows.Forms.GroupBox();
+            this.cbShowStep = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPoint = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtCountLine = new System.Windows.Forms.TextBox();
+            this.txtCountPoint = new System.Windows.Forms.TextBox();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblChoose = new System.Windows.Forms.Label();
+            this.picMap = new System.Windows.Forms.PictureBox();
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.grpControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).BeginInit();
+            this.grpInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpControl
+            // 
+            this.grpControl.Controls.Add(this.cbShowStep);
+            this.grpControl.Controls.Add(this.label1);
+            this.grpControl.Controls.Add(this.lblPoint);
+            this.grpControl.Controls.Add(this.btnFind);
+            this.grpControl.Controls.Add(this.btnGenerate);
+            this.grpControl.Controls.Add(this.txtCountLine);
+            this.grpControl.Controls.Add(this.txtCountPoint);
+            this.grpControl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grpControl.Location = new System.Drawing.Point(29, 25);
+            this.grpControl.Name = "grpControl";
+            this.grpControl.Size = new System.Drawing.Size(146, 269);
+            this.grpControl.TabIndex = 0;
+            this.grpControl.TabStop = false;
+            this.grpControl.Text = "Control";
+            // 
+            // cbShowStep
+            // 
+            this.cbShowStep.AutoSize = true;
+            this.cbShowStep.Location = new System.Drawing.Point(36, 169);
+            this.cbShowStep.Name = "cbShowStep";
+            this.cbShowStep.Size = new System.Drawing.Size(78, 17);
+            this.cbShowStep.TabIndex = 11;
+            this.cbShowStep.Text = "Show Step";
+            this.cbShowStep.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Line:";
+            // 
+            // lblPoint
+            // 
+            this.lblPoint.AutoSize = true;
+            this.lblPoint.Location = new System.Drawing.Point(22, 46);
+            this.lblPoint.Name = "lblPoint";
+            this.lblPoint.Size = new System.Drawing.Size(34, 13);
+            this.lblPoint.TabIndex = 9;
+            this.lblPoint.Text = "Point:";
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(36, 211);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 5;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(36, 118);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtCountLine
+            // 
+            this.txtCountLine.Location = new System.Drawing.Point(70, 77);
+            this.txtCountLine.MaxLength = 3;
+            this.txtCountLine.Name = "txtCountLine";
+            this.txtCountLine.Size = new System.Drawing.Size(55, 20);
+            this.txtCountLine.TabIndex = 1;
+            this.txtCountLine.Text = "10";
+            this.txtCountLine.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtCountPoint
+            // 
+            this.txtCountPoint.Location = new System.Drawing.Point(70, 43);
+            this.txtCountPoint.MaxLength = 2;
+            this.txtCountPoint.Name = "txtCountPoint";
+            this.txtCountPoint.Size = new System.Drawing.Size(55, 20);
+            this.txtCountPoint.TabIndex = 0;
+            this.txtCountPoint.Text = "5";
+            this.txtCountPoint.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDest.ForeColor = System.Drawing.Color.Green;
+            this.lblDest.Location = new System.Drawing.Point(24, 75);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(49, 18);
+            this.lblDest.TabIndex = 7;
+            this.lblDest.Text = "Dest: ";
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.ForeColor = System.Drawing.Color.DarkViolet;
+            this.lblStart.Location = new System.Drawing.Point(24, 43);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(50, 18);
+            this.lblStart.TabIndex = 6;
+            this.lblStart.Text = "Start: ";
+            // 
+            // lblChoose
+            // 
+            this.lblChoose.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChoose.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblChoose.Location = new System.Drawing.Point(13, 116);
+            this.lblChoose.Name = "lblChoose";
+            this.lblChoose.Size = new System.Drawing.Size(124, 16);
+            this.lblChoose.TabIndex = 8;
+            this.lblChoose.Text = "By Nguyen Khanh";
+            this.lblChoose.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picMap
+            // 
+            this.picMap.BackColor = System.Drawing.Color.White;
+            this.picMap.Location = new System.Drawing.Point(200, 20);
+            this.picMap.Name = "picMap";
+            this.picMap.Size = new System.Drawing.Size(750, 600);
+            this.picMap.TabIndex = 1;
+            this.picMap.TabStop = false;
+            this.picMap.Click += new System.EventHandler(this.picMap_Click);
+            this.picMap.Paint += new System.Windows.Forms.PaintEventHandler(this.picMap_Paint);
+            // 
+            // grpInfo
+            // 
+            this.grpInfo.Controls.Add(this.lblChoose);
+            this.grpInfo.Controls.Add(this.lblStart);
+            this.grpInfo.Controls.Add(this.lblDest);
+            this.grpInfo.Location = new System.Drawing.Point(29, 324);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Size = new System.Drawing.Size(146, 149);
+            this.grpInfo.TabIndex = 9;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Info";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 400;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(984, 641);
+            this.Controls.Add(this.grpInfo);
+            this.Controls.Add(this.picMap);
+            this.Controls.Add(this.grpControl);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TTCNTT";
+            this.grpControl.ResumeLayout(false);
+            this.grpControl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).EndInit();
+            this.grpInfo.ResumeLayout(false);
+            this.grpInfo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpControl;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtCountLine;
+        private System.Windows.Forms.TextBox txtCountPoint;
+        private System.Windows.Forms.PictureBox picMap;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblChoose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPoint;
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.CheckBox cbShowStep;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ TTCNTT/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace TTCNTT
+{
+    public partial class Form1 : Form
+    {
+        private const int W = 750, H = 600;
+        private const int radius = 20, radius2 = radius * 2, radius3 = radius + 3;
+        private const int size = 150;
+        private Random random = new Random();
+        private Font drawFont = new Font("Arial", 18);
+        private StringFormat sf = new StringFormat();
+        private Pen pen = new Pen(Color.Black) { Width = 2 };
+        private Pen penStart = new Pen(Color.DarkViolet) { Width = 4 };
+        private Pen penDest = new Pen(Color.LimeGreen) { Width = 4 };
+        private Pen penBlackArrow = new Pen(Color.Black) { Width = 2, CustomEndCap = new AdjustableArrowCap(5, 5, false) };
+        private Pen penStepArrow = new Pen(Color.Aqua) { Width = 3, CustomEndCap = new AdjustableArrowCap(3, 3, false) };
+        private Pen penPathArrow = new Pen(Color.Blue) { Width = 4, CustomEndCap = new AdjustableArrowCap(3, 3, false) };
+
+        private int countPoint, countLine;
+        private Point[] points = new Point[20];
+        private PairPoint[,] lines = new PairPoint[20, 20];
+        private int[,] distance = new int[20, 20];
+        private int[] newDistance = new int[20];
+
+        private int start = -1, dest = -1;
+        private Point[] listStep = new Point[400];
+        private int countStep = 0, maxStep = 0;
+        private List<int> listPath = new List<int>();
+        private bool isFounded = false;
+
+        private int chooseState = 0;
+        private string[] textChoose = { "By Nguyen Khanh", "Choose Start ...", "Choose Dest ..." };
+
+        public Form1()
+        {
+            InitializeComponent();
+            sf.LineAlignment = StringAlignment.Center;
+            sf.Alignment = StringAlignment.Center;
+
+            countPoint = Convert.ToInt32(txtCountPoint.Text);
+            countLine = Convert.ToInt32(txtCountLine.Text);
+            reset();
+            generate();
+        }
+
+        void reset()
+        {
+            start = dest = -1;
+            countStep = maxStep = 0;
+            listPath.Clear();
+            chooseState = 0;
+            lblChoose.Text = textChoose[chooseState];
+            lblStart.Text = "Start: ";
+            lblDest.Text = "Dest: ";
+            isFounded = false;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            reset();
+
+            bool ok = false;
+            try
+            {
+                countPoint = Convert.ToInt32(txtCountPoint.Text);
+                countLine = Convert.ToInt32(txtCountLine.Text);
+                if (countPoint > 0 && countPoint <= 20 && countLine > 0 && countLine <= countPoint * (countPoint - 1))
+                    ok = true;
+            }
+            catch { }
+            if (ok)
+                generate();
+            else
+                MessageBox.Show("Wrong amount of points", "Information");
+        }
+
+        void picMap_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            if (isFounded)
+            {
+                int old, n;
+                if (cbShowStep.Checked)
+                {
+                    for (int i = 0; i < countStep; i++)
+                    {
+                        old = listStep[i].Y;
+                        n = listStep[i].X;
+                        g.DrawLine(penStepArrow, lines[old, n].p1, lines[old, n].p2);
+                    }
+                    old = listStep[countStep].Y;
+                    n = listStep[countStep].X;
+                    g.DrawLine(penPathArrow, lines[old, n].p1, lines[old, n].p2);
+                }
+                if (countStep == maxStep-1)
+                {
+                    old = dest;
+                    foreach (int i in listPath)
+                    {
+                        if (i == dest)
+                            continue;
+                        g.DrawLine(penPathArrow, lines[i, old].p1, lines[i, old].p2);
+                        old = i;
+                    }
+                }
+            }
+
+            if (start != -1)
+                g.DrawEllipse(penStart, new Rectangle(points[start].X - radius, points[start].Y - radius, radius2, radius2));
+            if (dest != -1)
+                g.DrawEllipse(penDest, new Rectangle(points[dest].X - radius, points[dest].Y - radius, radius2, radius2));
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            countStep++;
+            picMap.Invalidate();
+            if (countStep == maxStep-1)
+            {
+                grpControl.Enabled = true;
+                timer.Enabled = false;
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            start = dest = -1;
+            isFounded = false;
+            picMap.Invalidate();
+            grpControl.Enabled = false;
+            chooseState = 1;
+            lblChoose.Text = textChoose[chooseState];
+        }
+
+        private void picMap_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (chooseState == 1)
+            {
+                for (int i = 0; i < countPoint; i++)
+                {
+                    if (me.X > points[i].X - radius && me.X < points[i].X + radius && me.Y > points[i].Y - radius && me.Y < points[i].Y + radius)
+                    {
+                        start = i;
+                        lblStart.Text = "Start: " + i;
+                        chooseState = 2;
+                        lblChoose.Text = textChoose[chooseState];
+                        picMap.Invalidate();
+                        break;
+                    }
+                }
+            }
+            else if (chooseState == 2)
+            {
+                for (int i = 0; i < countPoint; i++)
+                {
+                    if (me.X > points[i].X - radius && me.X < points[i].X + radius && me.Y > points[i].Y - radius && me.Y < points[i].Y + radius)
+                    {
+                        dest = i;
+                        lblDest.Text = "Dest: " + i;
+                        chooseState = 0;
+                        lblChoose.Text = textChoose[chooseState];
+                        picMap.Invalidate();
+
+                        if (dijkstra())
+                        {
+                            picMap.Invalidate();
+                            if (cbShowStep.Checked)
+                            {
+                                countStep = 0;
+                                timer.Enabled = true;
+                            }
+                            else
+                            {
+                                countStep = maxStep - 1;
+                                grpControl.Enabled = true;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Not found!", "Information");
+                            grpControl.Enabled = true;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        void generate()
+        {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            int count = 20;
+            int n, k = 0, region;
+            Point[] listLine = new Point[400];
+            Point p;
+            int offsetX, offsetY, px1, px2, py1, py2;
+            for (int i = 0; i < countPoint; i++)
+            {
+                for (int j = 0; j < countPoint; j++)
+                {
+                    distance[i, j] = 0;
+                    if (i != j)
+                    {
+                        listLine[k] = new Point(i, j);
+                        k++;
+                    }
+                }
+
+                n = random.Next(count);
+                region = array[n];
+                count--;
+                array[n] = array[count];
+                p = randomInRegion(region);
+                points[i] = p;
+            }
+            for (int i = 0; i < countLine; i++)
+            {
+                n = random.Next(k);
+                p = listLine[n];
+                k--;
+                listLine[n] = listLine[k];
+                distance[p.X, p.Y] = calDistance(points[p.X], points[p.Y]);
+                px1 = points[p.X].X;
+                px2 = points[p.Y].X;
+                py1 = points[p.X].Y;
+                py2 = points[p.Y].Y;
+                offsetX = (px2 - px1) * radius3 / distance[p.X, p.Y];
+                offsetY = (py2 - py1) * radius3 / distance[p.X, p.Y];
+                lines[p.X, p.Y] = new PairPoint() { p1 = new Point(px1 + offsetX, py1 + offsetY), p2 = new Point(px2 - offsetX, py2 - offsetY) };
+            }
+
+
+            Bitmap bitmap = new Bitmap(W, H);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.TextRenderingHint = TextRenderingHint.AntiAlias;
+            for (int i = 0; i < countPoint; i++)
+            {
+                for (int j = 0; j < countPoint; j++)
+                {
+                    if (distance[i, j] != 0)
+                    {
+                        g.DrawLine(penBlackArrow, lines[i, j].p1, lines[i, j].p2);
+                    }
+                }
+            }
+            for (int i = 0; i < countPoint; i++)
+            {
+                p = points[i];
+                Rectangle rect = new Rectangle(p.X - radius, p.Y - radius, radius2, radius2);
+                g.FillEllipse(Brushes.LightGray, rect);
+                g.DrawEllipse(pen, rect);
+                g.DrawString(i.ToString(), drawFont, Brushes.Black, p, sf);
+            }
+            picMap.Image = bitmap;
+        }
+
+        Point randomInRegion(int region)
+        {
+            int minX = region % 5 * size + radius, maxX = minX + size - radius2;
+            int minY = region / 5 * size + radius, maxY = minY + size - radius2;
+            return new Point(random.Next(minX, maxX), random.Next(minY, maxY));
+        }
+
+        static int calDistance(Point p1, Point p2)
+        {
+            float x = p1.X - p2.X, y = p1.Y - p2.Y;
+            return (int)Math.Sqrt(x * x + y * y);
+        }
+
+        bool dijkstra()
+        {
+            bool[] mark = new bool[20];
+            for (int i = 0; i < countPoint; i++)
+            {
+                mark[i] = false;
+                newDistance[i] = 0;
+            }
+            countStep = maxStep = 0;
+            listPath.Clear();
+
+            SortedSet<Element> queue = new SortedSet<Element>();
+            queue.Add(new Element(start, 0, null));
+            isFounded = false;
+            Element e;
+            while (queue.Count > 0)
+            {
+                e = queue.Min;
+                queue.Remove(e);
+                if (e.distance != newDistance[e.n])
+                    continue;
+                mark[e.n] = true;
+                if (e.prev != null)
+                {
+                    listStep[maxStep] = new Point(e.n, e.prev.n);
+                    maxStep++;
+                }
+                if (e.n == dest)
+                {
+                    while (true)
+                    {
+                        listPath.Add(e.n);
+                        e = e.prev;
+                        if (e == null)
+                            break;
+                    }
+                    isFounded = true;
+                    break;
+                }
+                for (int i = 0; i < countPoint; i++)
+                {
+                    if (!mark[i] && distance[e.n, i] > 0)
+                    {
+                        int dis = e.distance + distance[e.n, i];
+                        if (newDistance[i] == 0)
+                        {
+                            newDistance[i] = dis;
+                            queue.Add(new Element(i, dis, e));
+                        }
+                        else
+                        {
+                            if (newDistance[i] > dis)
+                            {
+                                newDistance[i] = dis;
+                                queue.Add(new Element(i, dis, e));
+                            }
+                        }
+                    }
+                }
+            }
+            return isFounded;
+        }
+    }
+
+    class PairPoint
+    {
+        public Point p1, p2;
+    }
+
+    class Element : IComparable<Element>
+    {
+        public int n, distance;
+        public Element prev;
+
+        public Element(int n = 0, int distance = 0, Element prev = null)
+        {
+            this.n = n;
+            this.distance = distance;
+            this.prev = prev;
+        }
+
+        public int CompareTo(Element other)
+        {
+            if (this == other)
+                return 0;
+            if (this.distance > other.distance)
+                return 1;
+            return -1;
+        }
+    }
+}
